4e38bd3278b064e1e671d5d6f81ca9c00b8b7be2
Niklas Heidloff <niklas_heidloff@de.ibm.com>
2018-11-28 17:37:16 +0100

    App ID setup

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Serverless Web Applications on the IBM Cloud
 
-This repo explains how to build a serverless web application in the IBM Cloud. Static web resources are stored in IBM Object Storage, business logic is implemented via IBM Cloud Functions, authentication is handled via IBM App ID and data is stored in the managed NoSQL database IBM Cloudant.
+This repo explains how to build serverless web applications in the IBM Cloud. Static web resources are stored in IBM Object Storage, business logic is implemented via IBM Cloud Functions, authentication is handled via IBM App ID and data is stored in the managed NoSQL database IBM Cloudant.
 
 The project contains two parts:
 
@@ -33,6 +33,7 @@ Make sure you have the following tools installed:
 * [git](https://git-scm.com/downloads)
 * [ibmcloud CLI](https://console.bluemix.net/docs/cli/index.html)
 * [Node](https://nodejs.org/en/download/)
+* [curl](https://curl.haxx.se/download.html)
 
 **Terminal Setup**
 
@@ -50,6 +51,19 @@ $ cat serverless-web-application.json
 
 In [local.env](local.env) define 'IBMCLOUD_API_KEY', 'IBMCLOUD_ORG', 'IBMCLOUD_SPACE' and 'BLUEMIX_REGION' to match the apikey in [serverless-web-application.json](serverless-web-application.json) and the org, space and region name that you're using (see the outputs in your terminal when following the steps above).
 
+**App ID Setup**
+
+Run the following command to create these artifacts:
+
+* APP ID service instance
+* APP ID credentials
+* APP ID test user (user@demo.email, verysecret)
+
+```
+$ scripts/setup-app-id.sh --setup
+```
+
+Copy 'APPID_TENANTID', 'APPID_OAUTHURL', 'APPID_CLIENTID' and 'APPID_SECRET' in [local.env](local.env).
 
 **to be continued**
 
---
+++ scripts/setup-app-id.sh
@@ -0,0 +1,140 @@
+#!/bin/bash
+
+##############################################################################
+# Copyright 2018 IBM Corporation
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# Authors: Andrea Frittoli, Niklas Heidloff
+##############################################################################
+
+root_folder=$(cd $(dirname $0); pwd)
+
+# SETUP logging (redirect stdout and stderr to a log file)
+readonly LOG_FILE="${root_folder}/deploy.log"
+readonly ENV_FILE="${root_folder}/../local.env"
+touch $LOG_FILE
+exec 3>&1 # Save stdout
+exec 4>&2 # Save stderr
+exec 1>$LOG_FILE 2>&1
+
+function _out() {
+  echo "$@" >&3
+  echo "$(date +'%F %H:%M:%S') $@"
+}
+
+function _err() {
+  echo "$@" >&4
+  echo "$(date +'%F %H:%M:%S') $@"
+}
+
+function check_tools() {
+    MISSING_TOOLS=""
+    git --version &> /dev/null || MISSING_TOOLS="${MISSING_TOOLS} git"
+    curl --version &> /dev/null || MISSING_TOOLS="${MISSING_TOOLS} curl"
+    ibmcloud --version &> /dev/null || MISSING_TOOLS="${MISSING_TOOLS} ibmcloud"    
+    if [[ -n "$MISSING_TOOLS" ]]; then
+      _err "Some tools (${MISSING_TOOLS# }) could not be found, please install them first and then run scripts/setup-app-id.sh"
+      exit 1
+    fi
+}
+
+function ibmcloud_login() {
+  # Skip version check updates
+  ibmcloud config --check-version=false
+
+  # Obtain the API endpoint from BLUEMIX_REGION and set it as default
+  _out Logging in to IBM cloud
+  ibmcloud api --unset
+  IBMCLOUD_API_ENDPOINT=$(ibmcloud api | awk '/'$BLUEMIX_REGION'/{ print $2 }')
+  ibmcloud api $IBMCLOUD_API_ENDPOINT
+
+  # Login to ibmcloud, generate .wskprops
+  ibmcloud login --apikey $IBMCLOUD_API_KEY -a $IBMCLOUD_API_ENDPOINT
+  ibmcloud target -o "$IBMCLOUD_ORG" -s "$IBMCLOUD_SPACE"
+  ibmcloud fn api list > /dev/null
+
+  # Show the result of login to stdout
+  ibmcloud target
+}
+
+function usage() {
+  _err -e "Usage: $0 [--setup,--env]"
+}
+
+function setup() {
+  _out Creating App ID service instance
+  ibmcloud resource service-instance-create app-id-serverless appid lite $BLUEMIX_REGION
+
+  _out Creating App ID credentials
+  ibmcloud resource service-key-create app-id-serverless-credentials Reader --instance-name app-id-serverless
+  ibmcloud resource service-key app-id-serverless-credentials
+  APPID_MGMTURL=$(ibmcloud resource service-key app-id-serverless-credentials | awk '/managementUrl/{ print $2 }')
+  APPID_TENANTID=$(ibmcloud resource service-key app-id-serverless-credentials | awk '/tenantId/{ print $2 }')
+  _out APPID_TENANTID: $APPID_TENANTID - copy this in local.env
+  APPID_OAUTHURL=$(ibmcloud resource service-key app-id-serverless-credentials | awk '/oauthServerUrl/{ print $2 }')
+  _out APPID_OAUTHURL: $APPID_OAUTHURL - copy this in local.env
+  APPID_CLIENTID=$(ibmcloud resource service-key app-id-serverless-credentials | awk '/clientId/{ print $2 }')
+  _out APPID_CLIENTID: $APPID_CLIENTID - copy this in local.env
+  APPID_SECRET=$(ibmcloud resource service-key app-id-serverless-credentials | awk '/secret/{ print $2 }')
+  _out APPID_SECRET: $APPID_SECRET - copy this in local.env
+  
+  
+  DEMO_EMAIL=user@demo.email
+  DEMO_PASSWORD=verysecret
+  _out Creating cloud directory test user: $DEMO_EMAIL, $DEMO_PASSWORD
+  IBMCLOUD_BEARER_TOKEN=$(ibmcloud iam oauth-tokens | awk '/IAM/{ print $3" "$4 }')
+  curl -s -X POST \
+    --header 'Content-Type: application/json' \
+    --header 'Accept: application/json' \
+    --header "Authorization: $IBMCLOUD_BEARER_TOKEN" \
+    -d '{"emails": [
+            {"value": "'$DEMO_EMAIL'","primary": true}
+          ],
+         "userName": "'$DEMO_EMAIL'",
+         "password": "'$DEMO_PASSWORD'"
+        }' \
+    "${APPID_MGMTURL}/cloud_directory/Users"
+}
+
+
+# Main script starts here
+check_tools
+
+# Load configuration variables
+if [ ! -f $ENV_FILE ]; then
+  _err "Before deploying, copy template.local.env into local.env and fill in environment specific values."
+  exit 1
+fi
+source $ENV_FILE
+export TF_VAR_ibm_bx_api_key=$IBMCLOUD_API_KEY
+export TF_VAR_ibm_cf_org=$IBMCLOUD_ORG
+export TF_VAR_ibm_cf_space=$IBMCLOUD_SPACE
+export IBMCLOUD_API_KEY BLUEMIX_REGION
+export TF_VAR_appid_plan=${IBMCLOUD_APPID_PLAN:-"lite"}
+export TF_VAR_cloudant_plan=${IBMCLOUD_CLOUDANT_PLAN:-"Lite"}
+
+WHAT_TO_DO=${1:-"usage"}
+
+case "$WHAT_TO_DO" in
+"--setup" )
+shift
+_out Full install output in $LOG_FILE
+ibmcloud_login
+setup $@
+;;
+"--env" )
+shift
+_err "==> Output of \"env\" command:"
+env $@ 4>&3 >&3
+;;
+esac
