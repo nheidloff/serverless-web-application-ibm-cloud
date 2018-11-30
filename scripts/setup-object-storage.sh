f3472b4ade80f9bbcaf06295dd22cbfb59b700be
Niklas Heidloff <niklas_heidloff@de.ibm.com>
2018-11-30 17:20:33 +0100

    change the base href

--- scripts/setup-object-storage.sh
+++ scripts/setup-object-storage.sh
@@ -92,7 +92,8 @@ function setup() {
   cd ${root_folder}/../angular
   ng build
 
-  npm --prefix ${root_folder}/text-replace start ${root_folder}/text-replace ${root_folder}/../angular/dist/index.html src=\" src=\"/${BUCKET_NAME}/
+  npm --prefix ${root_folder}/text-replace start ${root_folder}/text-replace ${root_folder}/../angular/dist/index.html src=\" src=\"https://s3.us-south.objectstorage.softlayer.net/${BUCKET_NAME}/
+  npm --prefix ${root_folder}/text-replace start ${root_folder}/text-replace ${root_folder}/../angular/dist/index.html <base href="/"> <script>document.write('<base href="' + document.location + '" />');</script>
 
   _out Uploading static web application resources
   curl -X "PUT" "https://s3.us-south.objectstorage.softlayer.net/${BUCKET_NAME}/index.html" \
